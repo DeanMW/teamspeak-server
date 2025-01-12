c2a0e3d70f00411bc751436bc8a150ac5a3a5b48
Dean Wake <deanmwake@gmail.com>
2025-01-12 13:23:22 +0200

    use correct permissions

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-aws cloudformation deploy \
-    --region eu-west-1 \
-    --stack-name teamspeak-server \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --template-file ./infra.yml \
-    --no-fail-on-empty-changeset \
